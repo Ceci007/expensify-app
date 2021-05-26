b0b5b4ecb5cfa19de266596d6d411ed7667ff46b
Ceci007 <cecibenitezca@gmail.com>
2021-05-26 14:29:33 -0400

    First draft of redux-expensify

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,15 +1,38 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0 }, action) => {
+const incrementCount = ({ incrementBy = 1 } = {}) => ({ 
+  type: 'INCREMENT',
+  incrementBy
+});
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+  type: 'DECREMENT',
+  decrementBy
+});
+
+const setCount = ({ count }) => ({
+  type: 'SET',
+  count
+});
+
+const resetCount = () => ({
+  type: 'RESET'
+});
+
+const countReducer = (state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
       return {
-        count: state.count + 1
+        count: state.count + action.incrementBy
       };
     case 'DECREMENT':
       return {
-        count: state.count - 1
+        count: state.count - action.decrementBy
       };
+    case 'SET':
+      return {
+        count: action.count
+      }
     case 'RESET':
       return {
         count: 0
@@ -17,24 +40,22 @@ const store = createStore((state = { count: 0 }, action) => {
     default:
       return state;
   }
+}
+
+const store = createStore(countReducer);
+
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
 });
 
-console.log(store.getState());
+store.dispatch(incrementCount({ incrementBy: 5 }));
 
-store.dispatch({
-  type: 'INCREMENT'
-});
+store.dispatch(incrementCount());
 
-store.dispatch({
-  type: 'INCREMENT'
-});
+store.dispatch(resetCount());
 
-store.dispatch({
-  type: 'RESET'
-});
+store.dispatch(decrementCount());
 
-store.dispatch({
-  type: 'DECREMENT'
-});
+store.dispatch(decrementCount({ decrementBy: 10 }));
 
-console.log(store.getState());
+store.dispatch(setCount({ count: 101 }));
