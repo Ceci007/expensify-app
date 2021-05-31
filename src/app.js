2ec9a30afcad8cace9b27a8c92217c213ff6921c
Ceci007 <cecibenitezca@gmail.com>
2021-05-31 12:16:56 -0400

    Controlled input for filters

--- src/app.js
+++ src/app.js
@@ -11,12 +11,12 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'water bill'}));
+store.dispatch(addExpense({ description: 'water bill', amount: 4500}));
 store.dispatch(addExpense({ description: 'gas bill'}));
 store.dispatch(setTextFilter('water'));
 
 setTimeout(() => {
-  store.dispatch(setTextFilter('rent'));
+  store.dispatch(setTextFilter('bill'));
 }, 3000);
 
 const state = store.getState();
