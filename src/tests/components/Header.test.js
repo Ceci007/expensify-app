701bb9eaf6014cd2b9d51c959bf45dc7386a4053
Ceci007 <cecibenitezca@gmail.com>
2021-06-11 12:49:17 -0400

    Snapshot testing with dynamic components

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Header from '../../components/Header';
 
-test('Should render header correctly', () => {
+test('Should render Header correctly', () => {
   const wrapper = shallow(<Header />);
   expect(wrapper).toMatchSnapshot();
 });
