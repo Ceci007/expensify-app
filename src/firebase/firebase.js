7866fb15b62dbc03262e0b7c1f4489294984377d
Ceci007 <cecibenitezca@gmail.com>
2021-06-15 14:12:26 -0400

    Using the firebase push method to add some expenses to the database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -14,6 +14,76 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
+database.ref('expenses').push({
+  description: 'Rent',
+  note: '',
+  amount: 109500,
+  createdAt: 8962360
+});
+
+database.ref('expenses').push({
+  description: 'Phone bill',
+  note: '',
+  amount: 300,
+  createdAt: 8962360
+});
+
+database.ref('expenses').push({
+  description: 'Food',
+  note: '',
+  amount: 1200,
+  createdAt: 8962360
+});
+
+/*
+const notes = [
+  {
+    id: '1',
+    title: 'Node.Js',
+    body: 'This a course on Node.Js'
+  },
+  {
+    id: '2',
+    title: 'React Fundamentals',
+    body: 'This is a course on React fundamentals'
+  },
+  {
+    id: '3',
+    title: 'Third Note',
+    body: 'This is my third note'
+  }
+];
+
+const onValueChange = database.ref().on('value', (snapshot) => {
+  const val = snapshot.val();
+  console.log(val);
+  console.log(`${val.name} is a ${val.job}!`);
+}, (error) => {
+  console.log('Failed to cancel subscription', error);
+});
+
+setTimeout(() => {
+  database.ref('age').set(25);
+}, 4000);
+
+setTimeout(() => {
+  database.ref().off(onValueChange);
+}, 8000);
+
+setTimeout(() => {
+  database.ref('age').set(30);
+}, 12000);
+*/
+/*
+database.ref().once('value')
+  .then((snapshot) => {
+    const val = snapshot.val();
+    console.log(val);
+  })
+  .catch((error) => {
+    console.log('Error fetching data', error);
+  });
+*/
 /*
 database.ref('isSingle')
   .remove()
@@ -24,6 +94,7 @@ database.ref('isSingle')
   });
 */
 
+/*
 database.ref().set({
   name: 'Cecilia Benitez',
   age: 25,
@@ -49,3 +120,4 @@ database.ref().update({
 }).catch((error) => {
   console.log('This failed', error);
 });
+*/
