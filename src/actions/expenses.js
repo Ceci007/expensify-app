124c41348256e337f1f3ce4ff4592d8b72e5df67
Ceci007 <cecibenitezca@gmail.com>
2021-06-17 10:16:33 -0400

    Broke tests

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,18 +1,30 @@
-import uuid from 'uuid';
+import database from '../firebase/firebase';
 
 // Add Expense Action
-export const addExpense = (
-  { description = '', note = '', amount = 0, createdAt = 0 } = {}
-) => ({
-  type: 'Add_EXPENSE',
-  expense: {
-    id: uuid(),
-    description,
-    note, 
-    amount,
-    createdAt
-  }
+export const addExpense = (expense) => ({
+  type: 'ADD_EXPENSE',
+  expense
 });
+
+export const startAddExpense = (expenseData = {}) => {
+  return (dispatch) => {
+    const {
+      description = '', 
+      note = '', 
+      amount = 0, 
+      createdAt = 0
+    } = expenseData;
+    const expense = { description, note, amount, createdAt };
+
+    return database.ref('expenses').push(expense)
+      .then((ref) => {
+        dispatch(addExpense({
+          id: ref.key,
+          ...expense
+        }));
+      });
+  };
+};
 
 // Remove Expense Action
 export const removeExpense = ({ id } = {}) => ({
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,17 +1,161 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyCTeoNwXPapveWsaDCOBX8pSnnXH9AtkVg",
-  authDomain: "expensify-b188b.firebaseapp.com",
-  databaseURL: "https://expensify-b188b-default-rtdb.firebaseio.com",
-  projectId: "expensify-b188b",
-  storageBucket: "expensify-b188b.appspot.com",
-  messagingSenderId: "821801296832",
-  appId: "1:821801296832:web:3c0ca99b1397ffc02ade25"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 firebase.initializeApp(config);
 
-firebase.database().ref().set({
-  name: 'Cecilia Benitez'
+const database = firebase.database();
+
+export { firebase, database as default }
+
+/*
+// child_removed
+database.ref('expenses').on('child_removed', (snapshot) => {
+  console.log(snapshot.key, snapshot.val());
 });
+
+// child_changed
+database.ref('expenses').on('child_changed', (snapshot) => {
+  console.log(snapshot.key, snapshot.val());
+});
+
+// child_added
+database.ref('expenses').on('child_added', (snapshot) => {
+  console.log(snapshot.key, snapshot.val());
+});
+
+const onValueChange = database.ref('expenses').on('value', (snapshot) => {
+  const expenses = [];
+
+  snapshot.forEach((childSnapshot) => {
+    expenses.push({
+      id: childSnapshot.key,
+      ...childSnapshot.val()
+    });
+  });
+
+  console.log(expenses);
+}, (error) => {
+  console.log('Failed to cancel subscription', error);
+});
+*/
+/*
+database.ref('expenses').once('value')
+  .then((snapshot) => {
+    const expenses = [];
+
+    snapshot.forEach((childSnapshot) => {
+      expenses.push({
+        id: childSnapshot.key,
+        ...childSnapshot.val()
+      });
+    });
+
+    console.log(expenses);
+  })
+  .catch((error) => {
+    console.log('error: ', error);
+  });
+*/
+/*
+database.ref('expenses').push({
+  description: 'Food',
+  note: '',
+  amount: 1200,
+  createdAt: 8962360
+});
+*/
+/*
+const notes = [
+  {
+    id: '1',
+    title: 'Node.Js',
+    body: 'This a course on Node.Js'
+  },
+  {
+    id: '2',
+    title: 'React Fundamentals',
+    body: 'This is a course on React fundamentals'
+  },
+  {
+    id: '3',
+    title: 'Third Note',
+    body: 'This is my third note'
+  }
+];
+
+const onValueChange = database.ref().on('value', (snapshot) => {
+  const val = snapshot.val();
+  console.log(val);
+  console.log(`${val.name} is a ${val.job}!`);
+}, (error) => {
+  console.log('Failed to cancel subscription', error);
+});
+
+setTimeout(() => {
+  database.ref('age').set(25);
+}, 4000);
+
+setTimeout(() => {
+  database.ref().off(onValueChange);
+}, 8000);
+
+setTimeout(() => {
+  database.ref('age').set(30);
+}, 12000);
+*/
+/*
+database.ref().once('value')
+  .then((snapshot) => {
+    const val = snapshot.val();
+    console.log(val);
+  })
+  .catch((error) => {
+    console.log('Error fetching data', error);
+  });
+*/
+/*
+database.ref('isSingle')
+  .remove()
+  .then(() => {
+    console.log('Data was removed');
+  }).catch((error) => {
+    console.log('Data did not changed', error);
+  });
+*/
+
+/*
+database.ref().set({
+  name: 'Cecilia Benitez',
+  age: 25,
+  isSingle: false,
+  location: {
+    city: 'Asuncion',
+    country: 'Paraguay'
+  }
+}).then(() => {
+  console.log('Data was saved');
+}).catch((error) => {
+  console.log('This failed', error);
+});
+
+database.ref().update({
+  name: 'Ceci',
+  age: 23,
+  job: 'Software Developer',
+  isSingle: null,
+  'location/city': 'Lambare'
+}).then(() => {
+  console.log('Data was updated');
+}).catch((error) => {
+  console.log('This failed', error);
+});
+*/
