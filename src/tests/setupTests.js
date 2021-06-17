31b0d203cb27bd9cead756681389ed193ad053a6
Ceci007 <cecibenitezca@gmail.com>
2021-06-17 16:37:11 -0400

    Added DotEnv import to setupTests.js file

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,7 +1,8 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-require('dotenv').config({ path: '.env.test' });
+import DotEnv from 'dotenv';
 
+DotEnv.config({ path: '.env.test' });
 // jest.setTimeout(30000);
 
 Enzyme.configure({
