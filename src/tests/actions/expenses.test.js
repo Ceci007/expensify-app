805f604f2e5fae02970247d1adaca4adb8ea8c0a
Ceci007 <cecibenitezca@gmail.com>
2021-06-18 14:26:43 -0400

    Added startRemoveExpense function and test

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -6,7 +6,8 @@ import {
   editExpense, 
   removeExpense,
   setExpenses,
-  startSetExpenses 
+  startSetExpenses,
+  startRemoveExpense 
 } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
@@ -26,6 +27,22 @@ test('should setup remove expense action object', () => {
   expect(action).toEqual({
     type: 'REMOVE_EXPENSE',
     id: '123abc'
+  });
+});
+
+test('Should remove expense from firebase', (done) => {
+  const store = createMockStore({});
+  const id = expenses[2].id;
+  store.dispatch(startRemoveExpense({ id })).then(() => {
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({
+      type: 'REMOVE_EXPENSE',
+      id
+    });
+    return database.ref(`expenses/${id}`).once('value');
+  }).then((snapshot) => {
+    expect(snapshot.val()).toBeFalsy();
+    done();
   });
 });
 
