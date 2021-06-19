9a64371a5875ba0c54fac8993bc814882855cc4e
Ceci007 <cecibenitezca@gmail.com>
2021-06-19 04:05:52 -0400

    Styled filters

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -66,6 +66,7 @@ export default class ExpenseForm extends React.Component {
         <form onSubmit={this.onSubmit}>
           <input 
             type="text"
+            className="text-input"
             placeholder="Description"
             autoFocus
             value={this.state.description}
@@ -73,6 +74,7 @@ export default class ExpenseForm extends React.Component {
           />
           <input 
             type="text"
+            className="text-input"
             placeholder="Amount"
             value={this.state.amount}
             onChange={this.onAmountChange}
@@ -86,6 +88,7 @@ export default class ExpenseForm extends React.Component {
             isOutsideRange={() => false}
           />
           <textarea 
+            className="textarea"
             placeholder="Add a note for your expense. (optional)"
             value={this.state.note}
             onChange={this.onNoteChange}
