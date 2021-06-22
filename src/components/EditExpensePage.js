ca44c5c7f9d2b12f27332131e5b126fe0ba75e07
Ceci007 <cecibenitezca@gmail.com>
2021-06-22 18:13:02 -0400

    Added modal for remove confirmation

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,46 +1,84 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
+import Modal from 'react-modal';
+import {startEditExpense, startRemoveExpense} from '../actions/expenses';
 import ExpenseForm from './ExpenseForm';
-import { startEditExpense, startRemoveExpense } from '../actions/expenses';
-
+ 
+ 
 export class EditExpensePage extends React.Component {
-  onSubmit = (expense) => {
-    this.props.startEditExpense(this.props.expense.id, expense);
-    this.props.history.push('/');
-  };
-
-  onRemove = () => {
-    this.props.startRemoveExpense({ id: this.props.expense.id });
-    this.props.history.push('/');
-  };
-
-  render() {
-    return (
-      <div>
-        <div className="page-header">
-          <div className="content-container">
-            <h1 className="page-header__title">Edit Expense</h1>
-          </div>
-        </div>
-        <div className="content-container">
-          <ExpenseForm 
-            expense={this.props.expense}
-            onSubmit={this.onSubmit}
-          />
-          <button className="button button--secondary" onClick={this.onRemove} >Remove Expense</button>
-        </div>
-      </div>
-    );
-  }
+ 
+    state = {
+        selectedOption: false
+    };
+ 
+    onSubmit = (expense) => {
+        this.props.startEditExpense(this.props.expense.id, expense);
+        this.props.history.push('/');
+    };
+ 
+    openModal = () => {
+        this.setState(() => ({selectedOption: true}));
+    };
+ 
+    closeModal = () => {
+        this.setState(() => ({selectedOption: false}));
+    };
+ 
+ 
+    onClick = () => {
+        this.props.startRemoveExpense({id: this.props.expense.id});
+        this.props.history.push('/');
+    };
+ 
+    render() {
+        return (
+            <div>
+                <div className="page-header">
+                    <div className="content-container">
+                        <h1 className="page-header__title">Edit Expense</h1>
+                    </div>
+                </div>
+                
+                <div className="content-container">
+                    <ExpenseForm 
+                        expense={this.props.expense}
+                        onSubmit={this.onSubmit}
+                    />
+                    <button className="button button--secondary" onClick={this.openModal}>Remove Expense</button>
+                </div>
+                <Modal
+                    isOpen={this.state.selectedOption}
+                    contentLabel="Removing Expense"
+                    onRequestClose={this.closeModal}
+                    closeTimeoutMS={200}
+                    className="modal"
+                >
+                    <p>Are you sure you want to remove the selected expense?</p>
+                    <div className="two-items">
+                        <button
+                          className="button button--danger" 
+                          onClick={this.onClick}>Remove</button>
+                        <button 
+                          className="button"
+                          onClick={this.closeModal}>Cancel</button>  
+                    </div>   
+                </Modal>
+            </div>
+        );
+    }
 }
-
-const mapStateToProps = (state, props) => ({
-  expense: state.expenses.find((expense) => (expense.id === props.match.params.id))
-});
-
-const mapDispatchToProps = (dispatch, props) => ({
-  startEditExpense: (id, expense) => dispatch(startEditExpense(id, expense)),
-  startRemoveExpense: (data) => dispatch(startRemoveExpense(data))
-});
-
+ 
+const mapDispatchToProps = (dispatch, props) => (
+    {
+        startEditExpense: (expenseId, expense) => dispatch(startEditExpense(expenseId, expense)),
+        startRemoveExpense: (data) => dispatch(startRemoveExpense(data))
+    }
+);
+ 
+const mapStateToProps = (state, props) => {
+    return {
+        expense: state.expenses.find((expense) => (expense.id === props.match.params.id))
+    };
+};  
+ 
 export default connect(mapStateToProps, mapDispatchToProps)(EditExpensePage);
